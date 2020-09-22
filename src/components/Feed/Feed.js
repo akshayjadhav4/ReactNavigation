247122bc267855c93e7a1fc0bb2acc56153acd91
AKSHAY <akshayjadhav461999@gmail.com>
2020-09-22 17:07:54 +0530

    stack Navigation working

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -2,11 +2,11 @@ import React from "react";
 import { View, Text, Button } from "react-native";
 import { styles } from "../../styles/styles";
 
-export default function Feed() {
+export default function Feed({navigation}) {
   return (
     <View style={styles.center}>
       <Text style={styles.title}>Navigation</Text>
-      <Button onPress={() => {}} title="Go To Feed" />
+      <Button onPress={() => navigation.navigate('Details')} title="Go To Feed" />
     </View>
   );
 }
