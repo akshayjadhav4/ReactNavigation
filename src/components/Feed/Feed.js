7fcc43636c82be039d8fe00214a15a8cc56d6673
AKSHAY <akshayjadhav461999@gmail.com>
2020-09-22 17:11:00 +0530

    code formatted

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -2,11 +2,15 @@ import React from "react";
 import { View, Text, Button } from "react-native";
 import { styles } from "../../styles/styles";
 
-export default function Feed({navigation}) {
+export default function Feed({ navigation }) {
   return (
     <View style={styles.center}>
       <Text style={styles.title}>Navigation</Text>
-      <Button onPress={() => navigation.navigate('Details')} title="Go To Feed" />
+      {/* onPress moving to Details screen  */}
+      <Button
+        onPress={() => navigation.navigate("Details")}
+        title="Go To Feed"
+      />
     </View>
   );
 }
