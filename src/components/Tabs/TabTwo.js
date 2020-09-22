7b7aa9658b6a6fbffe9fa6546ae519e7157346e9
AKSHAY <akshayjadhav461999@gmail.com>
2020-09-22 19:05:03 +0530

    useIsFocused

--- src/components/Tabs/TabTwo.js
+++ src/components/Tabs/TabTwo.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { View, Text } from "react-native";
 import { styles } from "../../styles/styles";
+import { useIsFocused } from "@react-navigation/native";
 
 export default function TabTwo() {
+  const isFocused = useIsFocused();
+  console.log("isFocused >>>>>>>>>>", isFocused);
   return (
     <View style={styles.center}>
       <Text style={styles.title}>Tab Two</Text>
