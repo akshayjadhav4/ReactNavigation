1d7f2ba37dbc5bd6cc24b60c9a68318747b3e563
AKSHAY <akshayjadhav461999@gmail.com>
2020-09-22 17:15:16 +0530

    Tab component created

---
+++ src/components/Tabs/TabThree.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { styles } from "../../styles/styles";
+
+export default function TabThree() {
+  return (
+    <View style={styles.center}>
+      <Text style={styles.title}>Tab Three</Text>
+    </View>
+  );
+}
